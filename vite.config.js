0c98333480f70fb7fae41a2f43fc2d95987ccddc
fathur-md <fathurmuh@outlook.com>
2026-01-10 07:07:05 +0700

    fix url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/hoax-detect-saas/",
+  base: "/hoaxdetect-ai-saas/",
   plugins: [react(), tailwindcss()],
 });
